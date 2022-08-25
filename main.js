62e14d51b1a362b90a3c537e73d3426adc16afbf
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-08-25 19:42:04 -0300

    functions async await, query el la URL

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 console.log("Hello");
+const limit = 3;
+const API_URL = `https://api.thecatapi.com/v1/images/search?limit=${limit}`;
 
-
-function refresh() {
-  const button = document.querySelector('button');
-  let img = document.querySelector('img');
-  button.disabled = true
-  const URL = "https://api.thecatapi.com/v1/images/search";
-  fetch(URL)
-  .then(res => res.json())
-  .then(data => {      
-    img.src = data[0].url;
-    console.log(data[0])
-    button.disabled = false
-  })  
+async function refresh() {
+  const button = document.querySelector("button");
+  let img1 = document.getElementById("image1");
+  let img2 = document.getElementById("image2");
+  let img3 = document.getElementById("image3");
+  button.disabled = true;
+  const result = await fetch(API_URL);
+  const data = await result.json();
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+  console.log(data[0]);
+  button.disabled = false;
 }
 refresh();
-
-
